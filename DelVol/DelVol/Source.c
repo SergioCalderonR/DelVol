6b6279338055c24b934eec8955c76df6ba2596de
SergioCalderonR <sercal@outlook.com>
2017-11-01 10:30:04 -0500

    Now you can specify which letter you want to unmount as argument.

--- DelVol/DelVol/Source.c
+++ DelVol/DelVol/Source.c
@@ -3,18 +3,65 @@
 
 #pragma comment(lib, "Kernel32.lib")
 
-LPWSTR AssignDriveLetter(DWORD logicalDrives)
+VOID ShowHelp(VOID)
 {
-	WCHAR *driveLetters[22] = {
+	wprintf(L"\nDelVol - Delete a volume letter and assign it a new one\n"
+			L"Copyrigth (C) 2017 Sergio Calderon\n"
+			L"\nUsage: DelVol.exe [VolumeLetter]\n" L"\nExample: DelVol.exe D:\\ \n");
+}
+
+BOOL ValidateLetter(LPWSTR driveLetter)
+{
+	BOOL isValid = TRUE;
+	if (_wcsicmp(driveLetter, L"A:\\") == 0 || _wcsicmp(driveLetter, L"B:\\")==0 ||
+		_wcsicmp(driveLetter, L"C:\\")==0)
+	{
+		isValid = FALSE;
+	}
+
+	return isValid;
+
+}
+
+LPWSTR GetVolumeLetter(LPWSTR letter, DWORD * pLetterIndex)
+{
+	
+	WCHAR *driveLetters[26] = {
+		L"A:\\", L"B:\\", L"C:\\", L"D:\\", L"E:\\", L"F:\\",
+		L"G:\\", L"H:\\", L"I:\\", L"J:\\", L"K:\\", L"L:\\",
+		L"M:\\", L"N:\\", L"O:\\", L"P:\\", L"Q:\\", L"R:\\",
+		L"S:\\", L"T:\\", L"U:\\", L"V:\\", L"W:\\", L"X:\\",
+		L"Y:\\", L"Z:\\" };
+	
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (_wcsicmp(letter, driveLetters[i]) == 0)
+		{
+			*pLetterIndex = i;
+		}
+	}
+
+	return letter;
+
+}
+
+LPWSTR AssignDriveLetter(DWORD logicalDrives, DWORD * pLetterIndex)
+{
+	WCHAR *driveLetters[23] = {L"D:\\",
 		L"E:\\", L"F:\\", L"G:\\", L"H:\\", L"I:\\",
 		L"J:\\", L"K:\\", L"L:\\", L"M:\\", L"N:\\",
 		L"O:\\", L"P:\\", L"Q:\\", L"R:\\", L"S:\\", L"T:\\",
 		L"U:\\", L"V:\\", L"W:\\", L"X:\\", L"Y:\\", L"Z:\\" };
 
-	int i = 0;	
+	int i = 0;
 
-	//Shift to letter "E:\"
-	logicalDrives >>= 4;
+	//If E:\ volume is passed, go back to D:\ to start over
+	if (*pLetterIndex == 4)
+	{
+		
+		logicalDrives <<= 1;
+	}
 
 	while (logicalDrives & 1)
 	{
@@ -48,31 +95,47 @@ VOID ShowError(DWORD errId)
 //Main function. All magic happens here! 
 int wmain(int argc, WCHAR * argv[])
 {
+	DWORD driveIndex=0;
+	PDWORD pDriveIndex = &driveIndex;
+
 	DWORD drivesOn;	
 	LPCWSTR volumeLetter=NULL;
+	
 
 	//GetVolumeNameForVolumeMountPoint
 	BOOL getVolume;
-	LPCWSTR oldDriveLetter = L"D:\\";
+	LPCWSTR oldDriveLetter=NULL;
 	WCHAR volumeGUID[50];
 	DWORD guidSize = 50;
 
 	//SetVolumeMountPoint
-	BOOL setVolume;	
+	BOOL setVolume;
+
+	if (argc != 2)
+	{
+		ShowHelp();
+		return FALSE;
+	}
+
+	if (!ValidateLetter(argv[1]))
+	{
+		fwprintf(stderr, L"\nWe cannot use drive letter %s\n", argv[1]);
+		return FALSE;
+	}
+
+	oldDriveLetter = GetVolumeLetter(argv[1], pDriveIndex);	
 
 	//Get all available logical drives
 	drivesOn = GetLogicalDrives();
 
-	if (drivesOn == 0)
+	drivesOn >>= *pDriveIndex;
+
+	if (drivesOn & 1)	//Drive is on! 
 	{
-		ShowError(GetLastError());
-	}
-	else if (drivesOn & 8)	//D drive is on! 
-	{
-		/*	We know the D is busy, so me need to
+		/*	We know the drive is busy, so me need to
 			get its volume GUID path, delete the 
 			volume mount point and set it with another
-			letter. I will use G.	*/		
+			letter. I will use next available letter.	*/		
 
 		//Here we get the volume GUID path
 		getVolume = GetVolumeNameForVolumeMountPointW(oldDriveLetter, volumeGUID, guidSize);
@@ -99,7 +162,7 @@ int wmain(int argc, WCHAR * argv[])
 
 		//AssignDriveLetter looks for the next available letter
 
-		setVolume = SetVolumeMountPointW(AssignDriveLetter(drivesOn), volumeGUID);
+		setVolume = SetVolumeMountPointW(AssignDriveLetter(drivesOn, pDriveIndex), volumeGUID);
 
 		if (!setVolume)	//Function failed
 		{
@@ -114,7 +177,7 @@ int wmain(int argc, WCHAR * argv[])
 	}
 	else
 	{
-		fwprintf(stderr, L"\nThere is not available drive.\n");
+		fwprintf(stderr, L"\nThere is not volume mounted with this letter.\n");
 		return FALSE;
 	}
 
